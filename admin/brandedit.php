79bee288d88b2dc422c78af333f02eb0edd99417
Imran <imufun1@gmail.com>
2016-07-20 00:10:10 +0600

    All file nicelly worked !! :)

---
+++ admin/brandedit.php
@@ -0,0 +1,56 @@
+<?php include 'inc/header.php';?>
+<?php include 'inc/sidebar.php';?>
+<?php include '../Classes/Brand.php';?>
+
+<?php
+
+
+    if (!isset($_GET['brandID']) || $_GET['brandID']==null) {
+         echo "<script>window.location = 'brandlist.php';</script>";
+    }else{
+        $id = $_GET['brandID'];
+    }
+        $brand  = new Brand();
+
+    if ($_SERVER['REQUEST_METHOD']== 'POST') {
+        $brandName = $_POST['brandName'];
+        $updateBrand = $brand->brandUpdate($brandName, $id);
+    }
+
+ ?>
+        <div class="grid_10">
+            <div class="box round first grid">
+                <h2>Update Category</h2>
+               <div class="block copyblock">
+                <?php
+                    if (isset($updateBrand)){
+                        echo $updateBrand;
+                    }
+                 ?>
+
+                 <?php
+                    $getBrand = $brand->getBrandByID($id);
+                     if ($getBrand) {
+                         while ($result = $getBrand->fetch_assoc()) {
+
+                 ?>
+                 <form method="post" action=" ">
+                    <table class="form" >
+                        <tr>
+                            <td>
+                                <input type="text" name="brandName" value="<?php echo $result['brandName'];?>" class="medium" />
+                            </td>
+                        </tr>
+						<tr>
+                            <td>
+                                <input type="submit" name="submit" Value="Save" />
+                            </td>
+                        </tr>
+                    </table>
+                    </form>
+
+                    <?php  } }?>
+                </div>
+            </div>
+        </div>
+<?php include 'inc/footer.php';?>
---
+++ admin/brandlist.php
@@ -0,0 +1,68 @@
+<?php include 'inc/header.php'; ?>
+<?php include 'inc/sidebar.php'; ?>
+<?php include '../Classes/Brand.php'; ?>
+
+<?php
+    $brand = new Brand();
+   if (isset($_GET['deleteBrand'])) {
+      $id = $_GET['deleteBrand'];
+       $branddel = $brand->delBrandById($id);
+  }
+?>
+
+<div class="grid_10">
+    <div class="box round first grid">
+        <h2>Brand List</h2>
+
+        <div class="block">
+
+
+            <?php
+            if (isset($branddel)) {
+                echo $branddel;
+            }
+
+            ?>
+            <table class="data display datatable" id="example">
+                <thead>
+                    <tr>
+                        <th>Serial No.</th>
+                        <th>Category Name</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <?php
+                    $getBrand = $brand->getAllBrand();
+                    if ($getBrand) {
+                        $i = 0;
+                        while ($result = $getBrand->fetch_assoc()) {
+                            $i++;
+                            ?>
+                            <tr class="odd gradeX">
+                                <td> <?php echo $i; ?> </td>
+                                <td> <?php echo $result['brandName']; ?> </td>
+                                <td>
+                                    <a href="brandedit.php?brandID=<?php echo $result['brandID']; ?>">Edit</a> ||
+                                    <a onclick="return confirm('Are you sure to delete!')" href="?deleteBrand=<?php echo $result['brandID']; ?>">Delete</a>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    <?php }
+                } ?>
+            </table>
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    $(document).ready(function () {
+        setupLeftMenu();
+
+        $('.datatable').dataTable();
+        setSidebarHeight();
+    });
+</script>
+<?php include 'inc/footer.php'; ?>
+
